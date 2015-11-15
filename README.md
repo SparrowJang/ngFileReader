9a7694bd9767c39421f6c5ea80884a75134e6218
Sparrow.jang <sparrow.jang@gmail.com>
2015-11-15 19:40:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,146 @@
+ngFileReader
+=============
+
+![demo image](/images/screenprint.png)
+
+This is file reader component on angular.
+
+##LiveDemo
+
+[demo](http://www.sparrowjang.com/example/bower_components/ngFileReader/example/)
+
+##Support
+
+* IE8+
+* chrome
+* firefox
+
+
+##Dependency
+
+* angularjs
+* jQuery
+* [FileReader](https://github.com/Jahdrien/FileReader)
+
+You need load **jQuery** and **FileReader** if browser is not support `FileReader`.
+
+And The base64 size only limit 32kb if you use base64 to set image on IE8.
+
+You also use other [FileReader](https://github.com/Phlow2001/FileReader) version,if you don't want to use `fixed` position by flash.
+
+##Install
+
+```bash
+bower install ngFileReader
+```
+
+##Usage
+
+Set some attrs of element.
+
+```html
+<div ng-file-reader on-readed="onReaded( event, file )" read-method="readMethod"  multiple></div>
+
+size:{{file.size | fileSize}}
+
+<img ng-src="{{img}}">
+```
+
+Set a reader method.
+
+```js
+$scope.readMethod = "readAsDataURL";
+```
+
+Add a `on-readed` callback to set file size and img data.
+
+```js
+$scope.onReaded = function( e, file ){
+
+  $scope.img = e.target.result;
+  $scope.file = file;
+};
+```
+
+##Attribute
+
+###filereader
+This is `swf` path, if you use ie8 ~ ie9.
+
+###multiple
+Input multiple property.
+
+###debug-mode
+Use `swf` only.
+
+**ex:**
+
+Set the attr on tag.
+
+```
+debug-mode="true"
+```
+
+###accept
+accept file extensions.
+
+##Parameter
+
+###read-method
+
+This is file reader method,when you selected file to call `read-method` method.
+
+**ex:** `readAsDataURL`、`readAsText`、`readAsBinaryString`
+
+Set the attr.
+
+```js
+read-method="readMethod"
+```
+
+Set a string value.
+
+```js
+$scope.readMethod = "readAsDataURL"
+```
+
+##Event
+
+###on-selected `Event`
+It will trigger this event if you has selected.
+
+###on-readed `Event`
+It will trigger this event When the selected file has readed by file reader.
+
+##Filter
+
+###fileSize
+
+Transfer file size from number to unit.
+
+```html
+<!-- 1048576 => 1 m -->
+{{file.size | fileSize}}
+```
+
+You can write this if you need round off to the 2nd decimal place.
+
+```html
+{{file.size | fileSize:2}}
+```
+
+##Run
+ 
+run a server:
+
+```bash
+grunt server
+```
+ 
+Finally,open your brower,enter [http://localhost:3000/example/index.html](http://localhost/example/index.html).
+
+
+##Minification
+
+Just run `npm install` to install dependencies. Then run `grunt` for minification.
+
---
+++ src/ngFileReader.js
@@ -0,0 +1,200 @@
+
+(function( angular ){
+
+  'use strict';
+
+  var app = angular.module( 'ngFileReader', [] );
+
+  app.directive( "ngFileReader", function(){
+
+    /**
+    * @param {Object} attrs
+    * @return Object
+    */
+    var parseParams_ = function( attrs ){
+
+      return {
+        multiple:attrs.multiple != undefined
+      };
+    };
+
+    /**
+    * @param {jQuery|jqlite} $elem
+    * @param {Object} inputParams
+    */
+    var init_ = function( $elem, inputParams ){
+
+      var $input = $elem.find( "input" );
+
+      if ( inputParams.multiple ) $input.attr( 'multiple', true );
+
+    };
+
+    var events = {
+
+      /**
+      * @param {Scope} scope
+      * @param {FileList} files
+      */
+      onSelected:function( scope, files ){
+
+        scope.onSelected( {files:files} );
+      },
+
+      /**
+      * @param {Scope} scope
+      * @param {FileList} files
+      */
+      onReadered:function( scope, files ){
+
+        angular.forEach( files, function( file ){
+
+          var readMethod = scope.readMethod;
+
+          if ( readMethod ) {
+
+            var fileReader = new FileReader();
+
+            fileReader.addEventListener( 'loadend', function( e ){
+
+              scope.onReaded( {event:e,file:file} );
+              scope.$apply();
+            });
+
+            fileReader[ readMethod ] && fileReader[ readMethod ]( file, scope.readEncoding );
+
+          }
+        });
+
+      }
+    };
+
+    return {
+
+      scope:{
+        onSelected:"&",
+        onReaded:"&",
+        readMethod:"=",
+        readEncoding:"=",
+        accept:"@",
+        filereader:"@"
+      },
+
+      template:"<input type='file' accept='{{accept}}' />",
+
+      /**
+      * @param {Scope} scope
+      * @param {jQuery|jqlite} $elem
+      * @param {Object} attrs
+      */
+      link:function( scope, $elem, attrs ){
+
+        var inputParams = parseParams_( attrs );
+
+        if ( window.File == undefined ) {
+
+          var position = $elem.css('position'),
+
+          initParams = {
+            multiple:inputParams.multiple,
+            debugMode:attrs.debugMode,
+            filereader:scope.filereader,
+          };
+
+          if ( position == "" || position == "static" ) {
+
+            $elem.css( "position", "relative" );
+
+            $.extend( initParams,{
+              appendTo:$elem,
+              position:"absolute",
+              offsetCss:function(){
+                return {left:0,top:0};
+              }
+            });
+
+          }
+
+          $elem.fileReader( initParams );
+
+
+        } else {
+
+          init_( $elem, inputParams );
+
+          var ignoreDrag = function( e ){
+
+            e.preventDefault();
+          };
+
+          $elem.on( "dragenter", ignoreDrag );
+          $elem.on( "dragover", ignoreDrag );
+          $elem.on( "drop", function( e ){
+
+              ignoreDrag( e );
+
+              //console.log( e.dataTransfer.files.length );
+
+              var originalEvent = e.originalEvent ? e.originalEvent : e,
+
+              files = originalEvent.dataTransfer.files;
+
+              //console.log( originalEvent );
+
+              events.onSelected( scope, files );
+
+              events.onReadered( scope, files );
+
+          });
+
+        }
+
+        $elem.on( 'change', function( evt ){
+
+          var files = evt.target.files;
+
+          events.onSelected( scope, files );
+
+          events.onReadered( scope, files );
+
+          $elem.find("input").val('');
+          //console.log( 'change' );
+          scope.$apply();
+        });
+
+      }
+    };
+
+  });
+
+  app.filter( 'fileSize', function(){
+
+      return function( total, bit ){
+
+          if ( !total ) return "";
+
+          var size_types = ["k","m","g","t"],
+              output = total,
+              index = -1;
+
+          while ( output >= 1024 ) {
+
+            var output = output / 1024;
+
+            index ++;
+
+            if ( output < 1 || index == 3 ) {
+
+              break;
+            }
+            
+          }
+
+          return output.toFixed( bit ? bit : 1 ) + " " + ( size_types[index]? size_types[index]:"" );
+
+      };
+  });
+
+
+})( angular );
+
